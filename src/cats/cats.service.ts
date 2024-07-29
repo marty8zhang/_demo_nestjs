e8245cf6cf2629e0a98652773db0a27da81188a8
Marty Zhang <marty8zhang@gmail.com>
2024-07-29 12:30:42 +1000

    Add exception handling to `CatsService.findAll()`.

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import {
+  // ForbiddenException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { UpdateCatDto } from './dto/update-cat.dto';
 
@@ -9,7 +14,37 @@ export class CatsService {
   }
 
   findAll() {
-    return `This action returns all cats`;
+    throw new HttpException(
+      `This action is supposed to return all cats, but it has been implemented yet.`,
+      HttpStatus.FORBIDDEN,
+    );
+    /*
+     * Method 2 - customised response body:
+    throw new HttpException(
+      {
+        message: `This action is supposed to return all cats, but it has been implemented yet.`,
+        statusCode: HttpStatus.FORBIDDEN,
+        additionalAttribute:
+          'Any arbitrary value can be added to the response body',
+      },
+      // When a response object is being provided, the status provided here will
+      // still be returned, but it won't be merged into the response body as a
+      // `statusCode` attribute as in the first example.
+      HttpStatus.FORBIDDEN,
+    );
+     */
+    /*
+     * Method 3 - use the built-in Nest HTTP exceptions:
+    throw new ForbiddenException(
+      `This action is supposed to return all cats, but it has been implemented yet.`,
+      {
+        cause: new Error(),
+        description:
+          'This optional `cause` object can provide additional error information to the response body as an `Error` attribute.',
+      },
+      // 'As an alternative to the `cause` object, this optional string is the other way to provide additional error information to the response body as an `error` attribute.'
+    );
+     */
   }
 
   findOne(id: number) {
