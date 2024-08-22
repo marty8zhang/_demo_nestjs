8dc222409b88b5c8b925f8bdc33fffd846f75b10
Marty Zhang <marty8zhang@gmail.com>
2024-08-22 15:55:21 +1000

    Create a Postgres migration to add a `title` field to the `user` table. The `User` entity hasn't been updated.
    Also, bump dependencies.

---
+++ migrations/configs/data-source.ts
@@ -0,0 +1,19 @@
+import { DataSource } from 'typeorm';
+import * as process from 'node:process';
+import { config as dotenvConfig } from 'dotenv';
+
+dotenvConfig({
+  path: `.env${!process.env.NODE_ENV ? '' : `.${process.env.NODE_ENV}`}`,
+});
+
+export const dataSource = new DataSource({
+  type: 'postgres',
+  host: `${process.env.POSTGRES_HOST}`,
+  port: 5432,
+  username: `${process.env.POSTGRES_USERNAME}`,
+  password: `${process.env.POSTGRES_PASSWORD}`,
+  database: `${process.env.POSTGRES_DATABASE}`,
+  entities: ['src/**/*.entity{.ts,.js}'],
+  migrations: ['migrations/*{.ts,.js}'],
+  synchronize: false,
+});
